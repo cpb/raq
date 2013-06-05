6ad2d1a2864208a591f408431a2a22afdad01736
Caleb Buxton <me@cpb.ca>
2013-06-04 20:35:32 -0700

    adding connection_options segment

--- lib/runner.rb
+++ lib/runner.rb
@@ -3,6 +3,7 @@ require 'optparse'
 class Runner
   attr_reader :options
   attr_reader :command
+  attr_reader :connection_options
 
   def initialize(argv)
     @argv = argv
@@ -15,12 +16,16 @@ class Runner
   def parser
     @parser ||= OptionParser.new do |opts|
       opts.on("-H", "--host HOST", "AMQP HOST address (default: #{@options[:host]}") { |host| @options[:host] = host }
+      opts.on("-p", "--port PORT", "AMQP PORT") { |port| @options[:port] = port.to_i }
       opts.on("-q", "--queue QUEUE","AMQP Queue to use") { |queue| @options[:queue] = queue }
+      opts.on("-u", "--user USER","AMQP User to connect as") { |user| @options[:user] = user }
+      # ... and on
     end
   end
 
   def parse!
     parser.parse! @argv
     @command = @argv.shift
+    @connection_options = @options.reject {|k,v| k == :queue}
   end
 end
--- spec/runner_spec.rb
+++ spec/runner_spec.rb
@@ -9,17 +9,58 @@ describe Runner do
     end
   end
 
-  it "should parse options" do
-    runner = described_class.new(argsv("start",{host: "127.0.0.1", queue: "example.queue"}))
+  context "options" do
+    it "should parse host" do
+      runner = described_class.new(%w(start --host 127.0.0.1))
 
-    expect(runner.options[:host]).to eql("127.0.0.1")
-    expect(runner.options[:queue]).to eql("example.queue")
+      expect(runner.options[:host]).to eql("127.0.0.1")
+
+      runner = described_class.new(%w(start -H host))
+
+      expect(runner.options[:host]).to eql("host")
+    end
+
+    it "should parse port" do
+      runner = described_class.new(%w(start --port 8080))
+
+      expect(runner.options[:port]).to eql(8080)
+
+      runner = described_class.new(%w(start -p 888))
+
+      expect(runner.options[:port]).to eql(888)
+    end
+
+    it "should parse user" do
+      runner = described_class.new(%w(start --user guest))
+
+      expect(runner.options[:user]).to eql("guest")
+
+      runner = described_class.new(%w(start -u gus))
+
+      expect(runner.options[:user]).to eql("gus")
+    end
+
+    # pass=>"[filtered]"
+    # auth_mechanism=>"PLAIN"
+    # vhost=>"/"
+    # timeout=>nil
+    # logging=>false
+    # ssl=>false
+    # broker=>nil
+    # frame_max=>131072
+    # heartbeat=>0}"
   end
 
   it "should parse specified command" do
     runner = described_class.new(argsv("start"))
 
     expect(runner.command).to eql("start")
+  end
+
+  it "should parse AMQP Connection options" do
+    runner = described_class.new(%w("start --host host.name --queue is.not.a.connection.option"))
+
+    expect(runner.connection_options).to_not include(:queue)
   end
 
   it "should abort on unknown command"
